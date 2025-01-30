f5f2a1cfaef5a6e122667c7441b142bc65ce5538
Your Name <you@example.com>
2025-01-30 19:15:53 +0300

    Add Linkdin Icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import linkedinIcon from '../assets/linkedin_2931621.png';
 
 const Card = (Props) => {
   return (
@@ -16,12 +17,10 @@ const Card = (Props) => {
                   </svg>
                 </a>
               )}
-              {Props.Linkdin && (
-                <a href={Props.Linkdin} target="_blank" rel="noopener noreferrer">
-                  <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-                    <path d="m416 32h-384.1c-17.6 0-31.9 14.5-31.9 32.3v383.4c0 17.8 14.3 32.3 31.9 32.3h384.1c17.6 0 32-14.5 32-32.3v-383.4c0-17.8-14.4-32.3-32-32.3zm-280.6 384h-66.4v-213.8h66.5v213.8zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5s17.2-38.5 38.5-38.5c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4v-104c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9v105.8h-66.4v-213.8h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9z"/>
-                  </svg>
-                </a>
+              {Props.linkdin && (
+                <a href={Props.linkdin} target="_blank" rel="noopener noreferrer">
+<img src={linkedinIcon} alt="LinkedIn Icon" className="svg" />
+</a>
               )}
             </div>
           </div>
@@ -171,9 +170,26 @@ const StyledWrapper = styled.div`
     font-size: 9px;
   }
 
+  .card .top-section .icons .social-media .svg,
+  .card .top-section .icons .social-media img {
+    height: 100%;
+    transition: filter 0.3s ease-in-out;
+  }
+
+  .card .top-section .icons .social-media .svg:hover,
+  .card .top-section .icons .social-media img:hover {
+    filter: brightness(0) invert(1);
+  }
+
+
+
+
+
   .card .bottom-section .row .item:nth-child(2) {
     border-left: 1px solid rgba(255, 255, 255, 0.126);
     border-right: 1px solid rgba(255, 255, 255, 0.126);
   }`;
 
+
+  
 export default Card;
