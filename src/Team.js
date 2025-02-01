d39845b213af4190d0b1ae9b6105ce1d4df4ec53
Your Name <you@example.com>
2025-02-01 22:45:33 +0300

    Change the TeamData

--- src/Team.js
+++ src/Team.js
@@ -1,37 +1,35 @@
 export const teamMembers = [
-    {
-      name: "Sultan Aldosari",
-      specialization: ' SWE',
-      skills: 'مهارات 1',
-      x: 'https://example.com',
-      linkdin: 'https://linkedin.com/in/example1',
-      github: 'https://github.com/SuL6aN502'
-    },
-    {
-      name: ' Fahad Alghamdi',
-      specialization: ' SWE',
-      skills: 'مهارات 2',
-      x: '',
-      linkdin: 'www.linkedin.com/in/fahad-alghamdi-a91969246',
-      github: 'https://github.com/3ntazDev'
-    },{
-    name: 'Khalid Alharbi',
-    specialization: ' SWE',
-    skills: 'مهارات 2',
-    x: 'https://example.com',
-    linkdin: 'https://linkedin.com/in/example2',
-    github: 'https://github.com/example2'
-  }, 
+  {
+    name: "Sultan Aldosari",
+    specialization: " Software Engineering",
+    skills: "مهارات 1",
+    x: "https://example.com",
+    linkdin: "https://linkedin.com/in/example1",
+    github: "https://github.com/SuL6aN502",
+  },
+  {
+    name: " Fahad Alghamdi",
+    specialization: " Software Engineering",
+    skills: "React developer",
+    x: "",
+    linkdin: "https://linkedin.com/in/fahad-alghamdi-a91969246",
+    github: "https://github.com/3ntazDev",
+  },
+  {
+    name: "Khalid Alharbi",
+    specialization: " Software Engineering",
+    skills: "مهارات 2",
+    x: "https://example.com",
+    linkdin: "https://linkedin.com/in/example2",
+    github: "https://github.com/example2",
+  },
 
-{
-  name: 'Saeed Almousa ',
-  specialization: 'تخصص 2',
-  skills: 'مهارات 2',
-  x: '',
-  linkdin: 'https://www.linkedin.com/in/saeedalmousa',
-  github: 'https://github.com/s3eedDev'
-},
-
-
-
+  {
+    name: "Saeed Almousa ",
+    specialization: "تخصص 2",
+    skills: "مهارات 2",
+    x: "",
+    linkdin: "https://www.linkedin.com/in/saeedalmousa",
+    github: "https://github.com/s3eedDev",
+  },
 ];
