42a074b3e567dbb026d22ccd6cca4578ad30a839
Your Name <you@example.com>
2025-02-02 23:45:43 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,38 @@
 import "../styles/Footer.css";
+import logo from "../assets/logo.png";
+import logo2 from "../assets/PSAU.png";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <p>&copy; 2023 Digital Innovation Club. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <img src={logo2} alt="جامعة الأمير سطام بن عبدالعزيز" className="footer-logo" />
+          <p>جامعة الأمير سطام بن عبدالعزيز</p>
+        </div>
+
+        <div className="footer-center">
+          <nav className="footer-links">
+            <a href="/contact">تواصل معنا</a>
+            <a href="/help">المساعدة الرئيسية</a>
+            <a href="/about">من نحن</a>
+          </nav>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com"><i className="fab fa-linkedin"></i></a>
+            <a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a>
+            <a href="https://www.facebook.com"><i className="fab fa-facebook"></i></a>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <img src={logo} alt="Digital Innovation Club" className="footer-logo" />
+          <p>DIGITAL INNOVATION CLUB</p>
+        </div>
+      </div>
+
+      <p className="footer-bottom">جميع الحقوق محفوظة لنادي الابتكار الرقمي © 2025</p>
+    </footer>
+  );
 };
 
 export default Footer;
-
